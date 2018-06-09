551f19ca86cebb0eb072fe9e31087a946db31ba1
Ragland <jakearagland@gmail.com>
2018-06-08 20:43:59 -0500

    still need to render pics in 4 x3 and onClick event with math.random.sort()

--- train-your-brain/src/App.js
+++ train-your-brain/src/App.js
@@ -1,12 +1,50 @@
 import React from "react";
 import Header from "./componets/Header";
-// import Section from "./components/Section";
+import Section from "./componets/Section";
 import Navbar from "./componets/Navbar";
+import imgages from "./img.json"; // [ { image: ''}, {image: ''}
+
 
 const App = () => (
   <div>
     <Navbar />
     <Header />
+    <Section
+      imgages={imgages[0]}
+     />
+         <Section
+      imgages={imgages[1]}
+     />
+         <Section
+      imgages={imgages[2]}
+     />
+         <Section
+      imgages={imgages[3]}
+     />
+         <Section
+      imgages={imgages[4]}
+     />
+         <Section
+      imgages={imgages[5]}
+     />
+         <Section
+      imgages={imgages[6]}
+     />
+         <Section
+      imgages={imgages[7]}
+     />
+         <Section
+      imgages={imgages[8]}
+     />
+         <Section
+      imgages={imgages[9]}
+     />
+         <Section
+      imgages={imgages[10]}
+     />
+         <Section
+      imgages={imgages[11]}
+     />
   </div>
 );
 
